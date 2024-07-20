e1e01f5335d572fd4c42028e68039b7df707d41b
Stephen Driffill <sdriffill@hotmail.co.uk>
2024-07-20 22:38:40 +0100

    Fix getWinner bug.

--- client/src/helpers.ts
+++ client/src/helpers.ts
@@ -11,23 +11,39 @@ export type Move = {
 export function getWinner(board: BoardState) {
   // check rows
   for (let i = 0; i < 3; i++) {
-    if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+    if (
+      board[i][0] !== undefined &&
+      board[i][0] === board[i][1] &&
+      board[i][0] === board[i][2]
+    ) {
       return board[i][0];
     }
   }
 
   // check columns
   for (let i = 0; i < 3; i++) {
-    if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+    if (
+      board[0][i] !== undefined &&
+      board[0][i] === board[1][i] &&
+      board[0][i] === board[2][i]
+    ) {
       return board[0][i];
     }
   }
 
   // check diagonals
-  if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+  if (
+    board[0][0] !== undefined &&
+    board[0][0] === board[1][1] &&
+    board[0][0] === board[2][2]
+  ) {
     return board[0][0];
   }
-  if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+  if (
+    board[0][2] !== undefined &&
+    board[0][2] === board[1][1] &&
+    board[0][2] === board[2][0]
+  ) {
     return board[0][2];
   }
 
